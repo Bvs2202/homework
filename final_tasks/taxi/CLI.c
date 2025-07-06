50e9510ebe5f4baded9f8e7cbbb822d0f6470a97
Batomunkuev Vladimir <Batomunkuev.vova@gmail.com>
2025-07-06 14:48:34 +0700

    123

--- final_tasks/taxi/CLI.c
+++ final_tasks/taxi/CLI.c
@@ -4,7 +4,12 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <sys/select.h>
+#include <signal.h>
 #include <time.h>
+
+#define MAX_DRIVERS 32
+#define MAX_CMD_LEN 32
+#define BUFFER_SIZE 64
 
 enum Status {
 	AVAILABLE_STATUS = 0,
@@ -29,10 +34,11 @@ int has_input()
 
 int create_driver(struct Driver *drivers)
 {
+	int pid = 0;
 	int sv[2] = {0};
-	char cmd_input[128] = {0};
-	char cmd_name[32] = {0};
-	char buff[32] = {0};
+	char cmd_input[MAX_CMD_LEN] = {0};
+	char cmd_name[MAX_CMD_LEN] = {0};
+	char buff[BUFFER_SIZE] = {0};
 	int arg1 = 0;
 	int arg2 = 0;
 	time_t rem_time, start_time, curr_time;
@@ -57,7 +63,7 @@ int create_driver(struct Driver *drivers)
 	else if (drivers->pid == 0)
 	{
 		close(sv[0]);
-		printf("pid = %d\n", drivers->fd_cli);
+		printf("create driver pid = %d\n", getpid());
 
 		while(1)
 		{
@@ -65,7 +71,7 @@ int create_driver(struct Driver *drivers)
 			memset(cmd_input, 0, sizeof(cmd_input));
 			arg1 = 0;
 			arg2 = 0;
-			read(sv[1], cmd_input, 128);
+			read(sv[1], cmd_input, MAX_CMD_LEN);
 
 			sscanf(cmd_input, "%32s %d %d", cmd_name, &arg1, &arg2);
 
@@ -88,7 +94,7 @@ int create_driver(struct Driver *drivers)
 				{
 					rem_time = drivers->task_timer - (time(NULL) - start_time);
 					memset(buff, 0, sizeof(buff));
-					sprintf(buff, "Busy %lu", rem_time);
+					snprintf(buff, BUFFER_SIZE, "Busy %lu", rem_time);
 					write(sv[1], buff, strlen(buff));
 				}
 			}
@@ -97,12 +103,12 @@ int create_driver(struct Driver *drivers)
 				memset(buff, 0, sizeof(buff));
 				if (drivers->status == AVAILABLE_STATUS)
 				{
-					sprintf(buff, "%s", "Available");
+					snprintf(buff, BUFFER_SIZE, "%s", "Available");
 				}
 				else if (drivers->status == BUSY_STATUS)
 				{
 					rem_time = drivers->task_timer - (time(NULL) - start_time);
-					sprintf(buff, "Busy %lu", rem_time);
+					snprintf(buff, BUFFER_SIZE, "Busy %lu", rem_time);
 				}
 				write(sv[1], buff, strlen(buff));
 			}
@@ -118,21 +124,27 @@ int create_driver(struct Driver *drivers)
 
 int main()
 {
-	struct Driver drivers[32] = {0};
+	struct Driver drivers[MAX_DRIVERS] = {0};
 	int n = 0;
-	char cmd_input[128] = {0};
-	char cmd_name[32] = {0};
-	char buff[32] = {0};
+	char cmd_input[MAX_CMD_LEN] = {0};
+	char cmd_name[MAX_CMD_LEN] = {0};
+	char buff[BUFFER_SIZE] = {0};
 	int arg1 = 0;
 	int arg2 = 0;
 	int fd_max = 0;
 	fd_set fds;
 
+	printf("Commands:\n");
+	printf("   create_driver\n");
+	printf("   send_task <pid> <task_timer>\n");
+	printf("   get_status <pid>\n");
+	printf("   get_drivers\n\n");
+
 	while (1)
 	{
 		FD_ZERO(&fds);
 		FD_SET(STDIN_FILENO, &fds);
-		for (int i = 0; i < 32; i++)
+		for (int i = 0; i < MAX_DRIVERS; i++)
 		{
 			if (drivers[i].fd_cli != 0)
 			{
@@ -166,7 +178,7 @@ int main()
 			{
 				if (strcmp("create_driver", cmd_name) == 0)
 				{
-					for (int i = 0; i < 32; i++)
+					for (int i = 0; i < MAX_DRIVERS; i++)
 					{
 						if (drivers[i].pid == 0)
 						{
@@ -186,29 +198,41 @@ int main()
 				}
 				else if (strcmp("send_task", cmd_name) == 0 && n == 3)
 				{
-					for (int i = 0; i < 32; i++)
+					int flag = 0;
+					for (int i = 0; i < MAX_DRIVERS; i++)
 					{
 						if (drivers[i].pid == arg1)
 						{
+							flag = 1;
 							write(drivers[i].fd_cli, cmd_input, strlen(cmd_input));
 							break;
 						}
 					}
+					if (flag == 0)
+					{
+						printf("The pid was not found.\n");
+					}
 				}
 				else if (strcmp("get_status", cmd_name) == 0)
 				{
-					for (int i = 0; i < 32; i++)
+					int flag = 0;
+					for (int i = 0; i < MAX_DRIVERS; i++)
 					{
 						if (drivers[i].pid == arg1)
 						{
+							flag = 1;
 							write(drivers[i].fd_cli, "get_status", strlen("get_status"));
 							break;
 						}
 					}
+					if (flag == 0)
+					{
+						printf("The pid was not found.\n");
+					}
 				}
 				else if (strcmp("get_drivers", cmd_name) == 0)
 				{
-					for (int i = 0; i < 32; i++)
+					for (int i = 0; i < MAX_DRIVERS; i++)
 					{
 						if (drivers[i].pid != 0)
 							write(drivers[i].fd_cli, "get_status", strlen("get_status"));
@@ -226,7 +250,7 @@ int main()
 			}
 		}
 
-		for (int i = 0; i < 32; i++)
+		for (int i = 0; i < MAX_DRIVERS; i++)
 		{
 			if (drivers[i].pid != 0 &&
 				FD_ISSET(drivers[i].fd_cli, &fds))
@@ -236,6 +260,16 @@ int main()
 					printf("index %d pid %d fd %d msg: %s\n",
 							i, drivers[i].pid, drivers[i].fd_cli, buff);
 			}
+		}
+	}
+
+	for (int i = 0; i < MAX_DRIVERS; i++)
+	{
+		if (drivers[i].pid != 0)
+		{
+			close(drivers[i].fd_cli);
+			kill(drivers[i].pid, SIGTERM);
+			waitpid(drivers[i].pid, NULL, 0);
 		}
 	}
 
